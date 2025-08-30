df4835c291a383c31a0a3b240286f1f579a1ef00
Haunt <zxcggfdrdg@gmail.com>
2025-08-30 04:10:06 -0700

    Update main.lua
    Added a fucking getplaceid's shit bro

--- apis/main.lua
+++ apis/main.lua
@@ -389,4 +389,23 @@ function mainAPI:StopTouchFling()
     end
 end
 
+function mainAPI:GetPlaceIDs()
+    -- fuck everyone for not showing me this shit, go fuck yourself
+    if not rconsoleprint then
+        replaceclosure(rconsoleprint, print)
+    end
+    rconsoleprint("Script By 17F7O\n\n")
+    local GPlaces = game:GetService("AssetService"):GetGamePlacesAsync()
+    while true do
+        for _, place in pairs(GPlaces:GetCurrentPage()) do
+            rconsoleprint("Name: " .. place.Name .. "\n")
+            rconsoleprint("PlaceId: " .. tostring(place.PlaceId) .. "\n")
+        end
+        if GPlaces.IsFinished then
+            break
+        end
+        GPlaces:AdvanceToNextPageAsync()
+    end
+end
+
 return mainAPI
